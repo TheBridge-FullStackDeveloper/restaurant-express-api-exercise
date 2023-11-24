812c5f780579cd29f4773257f2d52642463a9663
Mimimbres <miguel.nnez@gmail.com>
2023-11-24 17:18:54 +0100

    all done except for bonus

--- routes/orders.js
+++ routes/orders.js
@@ -1,7 +1,18 @@
-module.exports = 'app.js'
-const orders = "./data/orders.json"
+const orders = "../data/orders.json"
 const express = require('express');
-const app = express();
 const router = express.Router();
+const fs = require ("fs");
+
+router.get("/", (req, res) =>{
+    res.json(orders);
+});
+rouger.post("/", (req,res) => {
+    const newOrder = req.body;
+    const info = [...orders, newOrder]
+    fs.writeFile(".data/orders.json", JSON.stringify(info), (err) =>{
+        if (err) throw err;
+        res.json({message: "Order Created", data: newOrder});
+    });
+});
 
 module.exports = router
--- routes/plates.js
+++ routes/plates.js
@@ -1,25 +1,55 @@
 
 const plates =  require("../data/menu.json")
 const express = require('express');
-const app = express();
+const fs = require ("fs");
+const { stringify } = require("querystring");
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('¡Hola, mundo!');
+    res.json(plates);
   });
   router.post('/', (req, res) => {
     // Lógica para crear un recurso
-    res.send('Recurso creado exitosamente');
+ const plateNew= req.body;
+ const info = [...plates, plateNew];
+ fs.writeFile("./data/menu.json", JSON.stringify(info),(err) => {
+  if (err) throw err;
+  res.json ({message: "Plate Created", data: plateNew})
+ })
   });
 
-  router.put('/api/actualizar', (req, res) => {
+  router.put('/:id', (req, res) => {
     // Lógica para actualizar un recurso
-    res.send('Recurso actualizado exitosamente');
+    const plateUpdate = plates.map((el) => {
+      if (el.id === req.params.id){
+        return {
+          ...el,
+          name: req.body.name
+      }
+    }
+      return el;
+      
+    });
+    fs.writeFile("./data/menu.json", JSON.stringify(plateUpdate), (err) => {
+      if (err) throw err;
+      res.json({message: "Plate Updated", data: req.body.name})
+    })
   });
 
   router.delete('/api/borrar', (req, res) => {
     // Lógica para eliminar un recurso
-    res.send('Recurso eliminado exitosamente');
+    const plateDelete = plates.filter((el) => {
+      if (el.id === req.params.id){
+        return false;
+        } else{
+          return true
+      }
+    });
+    fs.writeFile("./data/menu.json", JSON.stringify(plateDelete),(err) => {
+      if (err) throw err;
+      res.json({message: "Plate removed", data: plates[1]});
+    });
+    
   });
   
   module.exports = router
