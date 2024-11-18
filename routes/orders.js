120f7c8bcb60bb1d0995e35eb184d3da2ac866f1
Raul Gamero <raul.a.gamero@gmail.com>
2024-11-18 21:21:04 +0100

    done

--- routes/orders.js
+++ routes/orders.js
@@ -11,41 +11,41 @@ router.get("/", (req, res) => {
 
 // Define a POST route to create a new order
 router.post("/", (req, res) => {
-    const newOrders = req.body.orders;
-    const newOrder = menu.filter((plate) => newOrders.includes(plate.id));
-    const data = JSON.stringify([...orders, { tableId: req.body.table, orders: newOrder}], null, 4);
-    fs.writeFile("./data/orders.json", data, (err) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        res.status(200).json({ message: "Order added successfully", data: JSON.parse(data) });
-    });
-})
+  const newOrders = req.body.orders;
+  const newOrder = menu.filter((plate) => newOrders.includes(plate.id));
+  const data = JSON.stringify([...orders, { tableId: req.body.table, orders: newOrder }], null, 4);
+  fs.writeFile("./data/orders.json", data, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    res.status(200).json({ message: "added correctly", data: JSON.parse(data) });
+  });
+});
 
 // Define a GET route to generate a bill for a specific table
 router.get("/bill/:tableId", (req, res) => {
-    const tableId = req.params.tableId;
-  
-    const [order] = orders.filter((el) => el.tableId === tableId);
-    const platesOrdered = order.orders.reduce(
-      (acc, order) => acc += `${order.name} ${order.price} \n`, ""
-    );
-  
-    const total = order.orders.reduce((acc, el) => (acc += el.price), 0);
-  
-    const totalString = `Total ${JSON.stringify(total, null, 4)}`;
-  
-    fs.writeFile(`./data/bill-${tableId}.txt`, platesOrdered + totalString, (err) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-  
-      res
-        .status(200)
-        .json({ message: "created correctly" });
-    });
+  const tableId = req.params.tableId;
+
+  const [order] = orders.filter((el) => el.tableId === tableId);
+  const platesOrdered = order.orders.reduce(
+    (acc, order) => acc += `${order.name} ${order.price} \n`, ""
+  );
+
+  const total = order.orders.reduce((acc, el) => (acc += el.price), 0);
+
+  const totalString = `Total ${JSON.stringify(total, null, 4)}`;
+
+  fs.writeFile(`./data/bill-${tableId}.txt`, platesOrdered + totalString, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    res
+      .status(200)
+      .json({ message: "created correctly" });
   });
+});
 
 module.exports = router;
--- routes/plates.js
+++ routes/plates.js
@@ -49,4 +49,6 @@ router.delete("/:id", (req, res) => {
     });
 });
 
+module.exports = router;
+
 
